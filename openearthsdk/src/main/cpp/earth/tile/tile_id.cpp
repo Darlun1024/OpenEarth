5fc15679ddb818ff9b7b33a24ab79e97885690eb
wusj <wusijie@geo-compass.com>
2018-01-08 11:17:03 +0800

    add boost

--- openearthsdk/src/main/cpp/earth/tile/tile_id.cpp
+++ openearthsdk/src/main/cpp/earth/tile/tile_id.cpp
@@ -2,4 +2,35 @@
 // Created by GXSN-Pro on 2018/1/3.
 //
 
+#include <tuple>
 #include "tile_id.hpp"
+#include <boost/functional/hash.hpp>
+
+namespace OpenEarth {
+    namespace Tile {
+        bool TileID::operator==(const TileID &rhs) const {
+            return x == rhs.x && y == rhs.y && z == rhs.z;
+        }
+
+        bool TileID::operator!=(const TileID &rhs) const {
+            return x != rhs.x || y != rhs.y || z != rhs.z;
+        }
+
+        bool TileID::operator<(const TileID &rhs) const {
+            return std::tie(z, x, y) < std::tie(rhs.z, rhs.x, rhs.y);
+        }
+
+        bool TileID::isChildOf(const TileID &rhs) const {
+
+        }
+
+    }
+}
+
+size_t std::hash<OpenEarth::Tile::TileID>::operator()(const OpenEarth::Tile::TileID &id)const {
+    size_t seed;
+    boost::hash_combine(seed,id.z);
+    boost::hash_combine(seed,id.x);
+    boost::hash_combine(seed,id.y);
+    return seed;
+}
--- openearthsdk/src/main/cpp/earth/tile/tile_id.hpp
+++ openearthsdk/src/main/cpp/earth/tile/tile_id.hpp
@@ -5,10 +5,34 @@
 #ifndef OPENEARTH_TILE_ID_HPP
 #define OPENEARTH_TILE_ID_HPP
 
+#include <cstdint>
 
-class TileID {
-    
+namespace OpenEarth{
+    namespace Tile{
+        class TileID {
+        public:
+            int32_t z;
+            int32_t y;
+            int8_t x;
+            TileID(int32_t x,int32_t y,int8_t z){
+                this->x = x;
+                this->y = y;
+                this->z = z;
+            }
+
+            ~TileID(){}
+            bool operator==(const TileID&) const;
+            bool operator!=(const TileID&) const;
+            bool operator<(const TileID&) const;
+            bool isChildOf(const TileID&) const;
+
+        };
+    }
+}
+
+template <>
+struct std::hash<OpenEarth::Tile::TileID> {
+    size_t operator()(const OpenEarth::Tile::TileID& id) const;
 };
-
 
 #endif //OPENEARTH_TILE_ID_HPP
